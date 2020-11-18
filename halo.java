ee5e7b161fe3628be9f7aac4dd46de702e4693c4
andita.rizkiah <anditaaa16@gmail.com>
2020-11-18 14:12:23 +0700

    ditambahkan isi

--- halo.java
+++ halo.java
@@ -1,7 +1,6 @@
 public class halo {
   public static void main(String[] args) {
     //Menampilkan String
-    System.out.println("Kunjungi http://www.tutorialpemrograman.com");
-    System.out.println("Terima kasih!");
+    System.out.println("Hello world!");
   }
 }
